70fa745e4bdbba433c51cfa8d07f2510cb95f291
Rohit Pillutla <67704466+BigRo26@users.noreply.github.com>
2025-08-14 13:16:44 -0500

    Update setup-env.bat

--- frontend/setup-env.bat
+++ frontend/setup-env.bat
@@ -1,5 +1,4 @@
 @echo off
-echo Creating .env file with your Firebase credentials...
 echo.
 
 echo REACT_APP_FIREBASE_API_KEY=AIzaSyClNdQC4Pgkt2PKazlnyUiPnDxBqFSwTlE > .env
@@ -10,7 +9,5 @@ echo REACT_APP_FIREBASE_MESSAGING_SENDER_ID=640519717396 >> .env
 echo REACT_APP_FIREBASE_APP_ID=1:640519717396:web:9253a9e2bff3f482165d01 >> .env
 
 echo.
-echo .env file created successfully!
-echo Your Firebase credentials are now secure and won't be committed to Git.
 echo.
 pause
